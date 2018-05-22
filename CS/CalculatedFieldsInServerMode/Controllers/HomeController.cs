563421794dabdd574c420c81c1ff4ca1b9fcc244
MikhailTymchukDX <MikhailTymchukDX@users.noreply.github.com>
2018-05-22 11:22:42 +0300

    first commit

---
+++ CS/CalculatedFieldsInServerMode/Controllers/HomeController.cs
@@ -0,0 +1,22 @@
+using DevExpress.Web.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CalculatedFieldsInServerMode.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [ValidateInput(false)]
+        public ActionResult GridViewPartial() {
+            return PartialView("_GridViewPartial");
+        }
+    }
+}
---
+++ CS/CalculatedFieldsInServerMode/Global.asax
@@ -0,0 +1,1 @@
+<%@ Application Codebehind="Global.asax.cs" Inherits="CalculatedFieldsInServerMode.MvcApplication" Language="C#" %>
